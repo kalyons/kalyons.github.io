e63677e08b69c11e1e9975eacf2ec24b77139541
kalyons <16kalyons@weymouthstudents.org>
2015-08-09 22:33:20 -0400

    Commit

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,6 +1,8 @@
 var metas = document.getElementsByTagName('meta');
 var i;
 var scrolling = false;
+var OGheight = null;
+var OGheight2 = null;
 if (navigator.userAgent.match(/iPhone/i)) {
   for (i=0; i<metas.length; i++) {
     if (metas[i].name == "viewport") {
@@ -17,16 +19,30 @@ function gestureStart() {
   }
 }
 
+function loadFunction() {
+  OGheight = $("#experiences").position().top;
+  OGheight2 = $("#achievements").position().top;
+}
+
 $(window).scroll(function() {
   if (!scrolling) {
-    if ($(this).scrollTop() > 400) {
-      $('#toTop').fadeIn();
-    } else {
-      $('#toTop').fadeOut();
+    if ($(this).scrollTop() > $("#achievements").position().top && $("#achievements").css('position') != 'fixed') {
+        //$("#experiences").css({'position':'static', 'top':'0px'});
+        $("#achievements").css({'position':'fixed', 'top':'0px', 'background-color':'#ffffff', 'width':'90%', 'padding':'0', 'margin':'0'});
+      }
+      else if ($(this).scrollTop() > $("#experiences").position().top && $("#experiences").css('position') != 'fixed') {
+        $("#experiences").css({'position':'fixed', 'top':'0px', 'background-color':'#ffffff', 'width':'90%', 'padding':'0', 'margin':'0'});
+        $("#toTop").fadeIn();
+      }
+      if ($(this).scrollTop() < OGheight2 && $("#achievements").css('position') == 'fixed') {
+        $("#achievements").css({'position':'static', 'top':'0px'});
+      }
+      if ($(this).scrollTop() < OGheight && $("#experiences").css('position') == 'fixed') {
+        $("#experiences").css({'position':'static', 'top':'0px'});
+      }
+      $height = window.innerHeight / 2 - 40;
     }
-    $height = window.innerHeight / 2;
-  }
-});
+  });
 
 function goToTop() {
   $('#toTop').fadeOut();
