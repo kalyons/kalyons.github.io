d2595fe52f1155f3cfff6635c71fe65c73d8c02a
kalyons <16kalyons@weymouthstudents.org>
2015-08-11 16:03:27 -0400

    Added Google Analytics functionality to all pages.

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,14 +1,14 @@
 var updatedAt = "Updated 08-11`-2015";
 var metas = document.getElementsByTagName('meta');
-var i;
+var num;
 var scrolling = false;
 var OGheight = null;
 var OGheight2 = null;
 var OGheight3 = null;
 if (navigator.userAgent.match(/iPhone/i)) {
-  for (i=0; i<metas.length; i++) {
-    if (metas[i].name == "viewport") {
-      metas[i].content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
+  for (num=0; num<metas.length; num++) {
+    if (metas[num].name == "viewport") {
+      metas[num].content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
     }
   }
   document.addEventListener("gesturestart", gestureStart, false);
@@ -33,7 +33,7 @@ function loadFunction() {
     OGheight3 = $("#skills").position().top;
   }
   catch (e) {
-    
+
   }
 }
 
